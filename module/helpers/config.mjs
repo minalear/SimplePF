3e49e1a207fdee6f5be84c254baea47680e2b6c4
Trevor Fisher <minalear@gmail.com>
2022-04-24 23:23:41 -0500

    Initial project commit with boilerplate system

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const SIMPLEPF = {};
+
+// Define constants here, such as:
+SIMPLEPF.foobar = {
+  'bas': 'SIMPLEPF.bas',
+  'bar': 'SIMPLEPF.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SIMPLEPF.abilities = {
+  "str": "SIMPLEPF.AbilityStr",
+  "dex": "SIMPLEPF.AbilityDex",
+  "con": "SIMPLEPF.AbilityCon",
+  "int": "SIMPLEPF.AbilityInt",
+  "wis": "SIMPLEPF.AbilityWis",
+  "cha": "SIMPLEPF.AbilityCha"
+};
+
+SIMPLEPF.abilityAbbreviations = {
+  "str": "SIMPLEPF.AbilityStrAbbr",
+  "dex": "SIMPLEPF.AbilityDexAbbr",
+  "con": "SIMPLEPF.AbilityConAbbr",
+  "int": "SIMPLEPF.AbilityIntAbbr",
+  "wis": "SIMPLEPF.AbilityWisAbbr",
+  "cha": "SIMPLEPF.AbilityChaAbbr"
+};
